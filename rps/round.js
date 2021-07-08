8f4bac59261efb361764eba8362d2a6a76d64beb
Johan Burke <jburke@milestechnologies.com>
2021-07-08 13:45:00 -0400

    Initial commit

---
+++ rps/round.js
@@ -0,0 +1,53 @@
+// Enum of Selections
+const Selections = {
+    ROCK: "rock",
+    PAPER: "paper",
+    SCISSORS: "scissors",
+};
+Object.freeze(Selections);
+
+// Enum of Outcomes
+const Outcomes = {
+    PLAYER_WINS: "player",
+    COMPUTER_WINS: "computer",
+    TIE: "tie",
+};
+Object.freeze(Outcomes);
+
+
+class Round{
+    playerSelection;
+    computerSelection;
+
+    constructor(playerSelection){
+        this.playerSelection = playerSelection;
+        this.computerSelection = this.getComputerSelection();
+    }
+
+    /**
+     * get a random computer selection
+     * @returns Selections
+     */
+    getComputerSelection() {
+        //*********************************ADD CODE HERE *************************************/
+        // Use Math.floor and select a random Selections enumerator from above 
+    }
+
+    /**
+     * determine the winner of the round
+     * @param {*} playerSelection
+     * @param {*} computerSelection
+     * @returns Outcomes
+     */
+    determineWinner() {
+        //*********************************ADD CODE HERE *************************************/
+        // Use this.playerSelection and this.computerSelection to return the appropriate outcome from the Outcomes enumerator above.
+    }
+
+
+
+}
+
+
+//DO NOT TOUCH THIS LINE OF CODE//
+let unit_test = Round;
